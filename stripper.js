2a137a9fca510f375bd8579cdfd340533df640b7
Ronald Roe <ronald.roe@gmail.com>
2014-02-08 13:43:05 -0600

    Added removal of unnecessary elements, added bookmarklet to Readme

--- stripper.js
+++ stripper.js
@@ -1,7 +1,14 @@
-var e=document.querySelectorAll('p, div, span');
-console.log(e);
-for (i=0, len=e.length; i<len; i++){
-	var t="<p>" + e[i].textContent + "</p>";
-	e[i].parentNode.removeChild(e[i]);
-	document.write(t);
+var rem=document.querySelectorAll('nav, *.nav, *.menu, footer, aside, header, body script, body style, body noscript, hr, img, iframe, embed, object, video, audio, canvas, map, area, svg, math, form, details, summary, menuitem, menu');
+var elem=document.querySelectorAll('p, h1, h2, h3, h4, h5, h6');
+
+console.log(rem);
+for (i=0, len=rem.length; i<len; i++){
+	rem[i].parentNode.removeChild(rem[i]);
+}
+
+console.log(elem);
+for (i=0, len=elem.length; i<len; i++){
+	var textContent="<p>" + elem[i].textContent + "</p>";
+	elem[i].parentNode.removeChild(elem[i]);
+	document.write(textContent);
 }
